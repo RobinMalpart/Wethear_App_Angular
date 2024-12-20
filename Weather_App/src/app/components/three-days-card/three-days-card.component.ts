26065997b1138fdfcbaf3e29b55a929ad46a47da
Sara Bevilacqua <sarabev6@gmail.com>
2024-12-20 13:46:05 +0100

    fix: move toggleFavorites button to cards

--- Weather_App/src/app/components/three-days-card/three-days-card.component.ts
+++ Weather_App/src/app/components/three-days-card/three-days-card.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
-
+import { Component, Input, SimpleChanges, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-three-days-card',
   templateUrl: './three-days-card.component.html',
@@ -11,14 +10,17 @@ export class ThreeDaysCardComponent {
   @Input() mainTemperature: number = 0;
   @Input() weatherDescription: string = '';
   @Input() currentWeatherIconUrl: string = '';
-
   @Input() threeDaysWeather: any[] = [];
+  @Input() unit: string = 'C';
+  @Input() isFavorite: boolean = false;
+  @Input() userId: string = '';
+  @Input() locationId: string = '';
+
+  @Output() toggleFavorite = new EventEmitter<{ userId: string; locationId: string }>();
 
   firstDayWeather: any[] = [];
   secondDayWeather: any[] = [];
   thirdDayWeather: any[] = [];
-
-  @Input() unit: string = 'C';
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['threeDaysWeather'] && this.threeDaysWeather.length > 0) {
@@ -34,5 +36,9 @@ export class ThreeDaysCardComponent {
     this.firstDayWeather = this.threeDaysWeather.slice(0, 8);
     this.secondDayWeather = this.threeDaysWeather.slice(8, 16);
     this.thirdDayWeather = this.threeDaysWeather.slice(16, 24);
+  }
+
+  onToggleFavorite(): void {
+    this.toggleFavorite.emit({ userId: this.userId, locationId: this.locationId });
   }
 }
