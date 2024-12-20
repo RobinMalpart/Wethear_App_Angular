302d07a0900c721800c4f5ca123eba0118d8d70c
Sara Bevilacqua <sarabev6@gmail.com>
2024-12-20 12:23:24 +0100

    fix: remove data from db.json

--- Weather_App/src/app/pages/home/home.component.ts
+++ Weather_App/src/app/pages/home/home.component.ts
@@ -73,7 +73,7 @@ export class HomeComponent implements OnInit {
       }
   
       // Last searched cities
-      const userId = '1a2b3c4d-5e6f-7g8h-9i0j-1k2l3m4n5o6p';
+      const userId = '1';
   
       if (!userId) {
         this.isNotUserConnected = true;
